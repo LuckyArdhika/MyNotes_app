904a774839bf4c8c440becef9b16ee0311b35bcb
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2021-07-12 16:56:13 +0700

    Update create_database.js

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -12,11 +12,25 @@ connection.query('CREATE DATABASE ' + dbconfig.database);
 connection.query('\
 CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `username` VARCHAR(20) NOT NULL, \
+    `email` VARCHAR(20) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
+    `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+    `isVerified` tinyint(2) NULL, \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
+    UNIQUE INDEX `email_UNIQUE` (`username` ASC) \
+)');
+
+connection.query('\
+CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.notes_table + '` ( \
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+    `title` VARCHAR(50) NOT NULL, \
+    `note` VARCHAR(3000) NOT NULL, \
+    `email` VARCHAR(20) NOT NULL, \
+    `isVerified` tinyint(2) NULL, \
+        PRIMARY KEY (`id`), \
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
+    UNIQUE INDEX `email_UNIQUE` (`username` ASC) \
 )');
 
 console.log('Success: Database Created!')
