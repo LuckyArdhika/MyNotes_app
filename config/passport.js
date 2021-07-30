0b0bba43b0becc311ad76580b324b1a28223e82c
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2021-07-31 05:56:08 +0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,17 @@
 // config/passport.js
-
+'use strict';
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
 // load up the user model
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var dbconfig = require('./database');
-const nodemailer = require("nodemailer");
+const linkVer = '/entermail';
 
 var connection = mysql.createConnection(dbconfig.connection);
 connection.query('USE ' + dbconfig.database);
 // expose this function to our app using module.exports
-module.exports = function(passport) {
+module.exports = function(passport, app) {
 
     // =========================================================================
     // passport session setup ==================================================
@@ -62,45 +62,9 @@ module.exports = function(passport) {
                     };
 // inserting user and password
 // inserting default note ("Welcome to the MyNotes")
-                    async function mailer(){
-
-                        let host = "localhost:3000";
-
-                        // create reusable transporter object using the default SMTP transport
-                        let transporter = nodemailer.createTransport({
-                        host: "smtp.gmail.com",
-                        port: 465,
-                        secure: true, // true for 465, false for other ports
-                        auth: {
-                            user: 'mynotesverify@gmail.com',// email
-                            pass: 'SAVAGE110',// password
-                            expires: 2000,
-                        },
-                        });
-                    
-                        // send mail with defined transport object
-                        let info = await transporter.sendMail({
-                            from: '"MyNotes Email Verification" <mynotesverify@mynotes.com>', // sender address
-                            to: req.body.username, // list of receivers // no receipent defined
-                            subject: "Verify Your Email", // Subject line
-                            // text: "Test text", // plain text body
-                            html: "<p>Hi "+req.body.username+", dont share this code to another human, include <b>MyNotes</b> team!</p></br>Your Code Is:"+rand, // html body
-                            });
-                    
-                        console.log("Message sent: %s", info.messageId);
-                        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-                        // khusus link verifikasi
-                        // if ((req.protocol + "://" + req.get('host')) == ("http://"+ host)){
-                        //     if(req.query.id == rand){
-                        //         connection.query("UPDATE users SET isVerified=? WHERE email=?",[1, newUserMysql.username]);
-                        //     }
-                        // }
-                        }
 
                     // verified=1, unverifiy=null
                     var insertQuery = "INSERT INTO users ( email, password ) values (?,?)";
-                    mailer().rand; //memanggil function mailer
                     connection.query("INSERT INTO notes ( id, title, note, email ) values ('','Welcome To The MyNotes','This is example of your note, note that you reading terms and use!',?)", [newUserMysql.username]),
                     connection.query(insertQuery,[newUserMysql.username, newUserMysql.password],function(err, rows) {
                         newUserMysql.id = rows.insertId;
@@ -116,7 +80,7 @@ module.exports = function(passport) {
     // =======================================================================; 21e,o.l[12.]==
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
-
+    
     passport.use(
         'local-login',
         new LocalStrategy({
@@ -127,34 +91,52 @@ module.exports = function(passport) {
         },
         function(req, username, password, done) { // callback with email and password from our form
             connection.query("SELECT * FROM users WHERE email = ?",[username], function(err, rows){
+                // rows[0] is a data for session(serialize);
                 if (err)
                     return done(err);
                 if (!rows.length) {
                     return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
                 }
 
-                // // kesalahan debugging, kode ditunda Error: Failed to serialize user into session
-                // connection.query('SELECT isVerified FROM users WHERE email=?',[username], (err, results, rows) => {
-                //     console.log(rows)
-                //         if (err)
-                //         throw err;
-                //         if (rows.isVerified == 1){
-                //             return done(false, req.flash('verifyMessage', 'Please verify your email first!'));
-                //         } else {
-                //             return done(null, rows[0]);
-                //         } //bagaimana cara mengidentifikasi null dan int di mysql dengan javascript?
-                // });
+                
+                
+                // if the user is found but the password is wrong
+                if (!bcrypt.compareSync(password, rows[0].password)) //mencocokkan password kedalam database yang di hash
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                // all is well, return successful user
+                
+                let datausr = rows[0]; // defined serialize data user info login
 
-                    // if the user is found but the password is wrong
-                    if (!bcrypt.compareSync(password, rows[0].password)) //mencocokkan password kedalam database yang di hash
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-                    // all is well, return successful user
-                    return done(null, rows[0]);
+                // seleksi value isVerified
+                connection.query('SELECT isVerified FROM users WHERE email=?',[username], (err, results, rows) => {
+                    console.log(results); //json row data packet of isVerified
+                    // console.log(rows);
+                    try {
+                    let parseR = JSON.stringify(results); //result harus dijadikan string dahulu
+                    let hasil = JSON.parse(parseR); //lalu di parser
+                    console.log(hasil); //menampilkan hasil parser json (object)
+                    let kpy = Object.values(hasil[0]) // mengambil array di dalam object
+                    console.log(kpy); // null/1? null=unverified 1=verified
+                        if (kpy == 1){ // mengecek apakah value sama dengan 1?
+                            console.log('uwis verifikasi');
+                            return done(null, datausr); // Error: Failed to serialize user into session, why place in if conditional
+                        } else {
+                        console.log('urg verifikasi');
+                        return done(null, false, req.flash('loginMessage', 'Please verify your email first! Click <a href="'+ linkVer+'">Here</a>')); // cara menaruh link di flash message?
+                        // menampilkan button/link untuk menuju ke halaman verifikasi
+                        }
+                    } catch (err) {
+                        console.log('error mas e');
+                        throw err;
+                    }
+                }
+            );
+
                 });
             })
     );
 };
 
-// code verivication declared
+// code verivication declared & exporting to routes.js
 let rand = Math.floor((Math.random()* 120)+ 12);
 module.exports.rand = rand
