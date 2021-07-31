d1e681e39eee36b962b2a094ab5699268dcdb214
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2021-07-31 18:59:07 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'containers-us-west-11.railway.app',
+        'host': 'localhost',
         'user': 'root',
-        'password': 'QNLIqM0L2CORHIeu4bxX',
-	'port': '5458'
+        'password': '',
+	'port': '3306'
     },
-	'database': 'railway',
-    'users_table': 'users',
-    'notes_table': 'notes'
+	'database': 'mynotes',
+    	'users_table': 'users',
+    	'notes_table': 'notes'
 };
